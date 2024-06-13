d53af25f1fc93fce269a6e1cf2bc744fa5ce22da
Sunil Gawai <sunilgawai7397@gmail.com>
2024-06-14 00:22:02 +0530

    feat: home:screen floating menu added

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,11 @@
-import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
-import ReusableButton from "@/components/button";
-import { PrimaryText, SecondaryText } from "@/components/text";
+import { StyleSheet, View } from "react-native";
+import { SecondaryText } from "@/components/text";
+import CardsMenu from "@/components/ui/floating-menu";
 
 const HomeScreen = () => {
-	const nav = useNavigation();
 	return (
 		<View style={styles.container}>
-			<PrimaryText>Join Us to Kick Start Your Learning</PrimaryText>
-			<SecondaryText>
-				Join and Learn from our AI Generated Instructions
-			</SecondaryText>
-			<ReusableButton
-				buttonStyle={{ backgroundColor: "black" }} // Custom button style
-				textStyle={{ color: "white" }} // Custom text style
-				buttonText="Sign In"
-				onPress={() => nav.navigate("Signin")}
-				containerStyle={{ marginTop: 50 }}
-			/>
-			<ReusableButton
-				buttonStyle={{ backgroundColor: "white" }} // Custom button style
-				textStyle={{ color: "black" }} // Custom text style
-				buttonText="Sign Up"
-				onPress={() => nav.navigate("Signup")}
-				containerStyle={{ marginTop: 10 }}
-			/>
+			<CardsMenu />
 		</View>
 	);
 };
@@ -37,5 +17,6 @@ const styles = StyleSheet.create({
 		flex: 1,
 		paddingTop: 35,
 		alignItems: "center",
+		// backgroundColor: "green",
 	},
 });
