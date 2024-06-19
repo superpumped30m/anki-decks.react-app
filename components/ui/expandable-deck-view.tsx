7c77d65153be3d749e439f9a129f2606da67d010
Sunil Gawai <sunilgawai7397@gmail.com>
2024-06-19 14:49:05 +0530

    decks & subdecks toggle successful

---
+++ components/ui/expandable-deck-view.tsx
@@ -0,0 +1,81 @@
+import { DeckType } from "@/data";
+import { View } from "../Themed";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { LayoutAnimation, StyleSheet, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useAppDispatch } from "@/store/hooks";
+import { toggleDeckExpand } from "@/store/deckSlice";
+
+type ExpandableDeckViewProps = {};
+
+const ExpandableDeckView = ({
+	id,
+	title,
+	sets,
+	isExpanded,
+	childrens,
+}: DeckType) => {
+	const dispatch = useAppDispatch();
+
+	const handlePress = () => {
+		LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+		dispatch(toggleDeckExpand(id));
+	};
+
+	return (
+		<View>
+			<TouchableOpacity style={styles.item} onPress={handlePress}>
+				{isExpanded ? (
+					<Ionicons name="chevron-down-outline" size={20} color={"black"} />
+				) : (
+					<Ionicons name="chevron-forward-outline" size={20} color={"black"} />
+				)}
+				<Text style={styles.itemText}>{title}</Text>
+			</TouchableOpacity>
+
+			<View style={styles.deckSeparator} />
+
+			{isExpanded && (
+				<View>
+					{childrens.map((child, idx) => (
+						<ExpandableDeckView {...child} key={idx} />
+					))}
+				</View>
+			)}
+		</View>
+	);
+};
+
+export default ExpandableDeckView;
+
+const styles = StyleSheet.create({
+	item: {
+		backgroundColor: "white",
+		padding: 20,
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 10,
+	},
+	itemText: {
+		fontSize: 16,
+		fontWeight: "500",
+	},
+	deckSeparator: {
+		height: 0.5,
+		backgroundColor: "#c8c8c8",
+	},
+	content: {
+		backgroundColor: "#f9f9f9",
+		padding: 15,
+		marginVertical: 5,
+	},
+	text: {
+		fontSize: 14,
+	},
+	separator: {
+		height: 0.5,
+		backgroundColor: "#c8c8c8",
+		marginVertical: 10,
+	},
+});
