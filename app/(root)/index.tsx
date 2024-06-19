4145067e40cf544359cbcf6f47731380b8ef9425
Sunil Gawai <sunilgawai7397@gmail.com>
2024-06-19 14:52:42 +0530

    removed unused components

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -1,14 +1,12 @@
 import { StyleSheet } from "react-native";
-import React, { useState } from "react";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import AccordianList from "@/components/ui/accordian-list";
 import FloatingMenu from "@/components/ui/floating-menu";
 import DecksList from "@/components/ui/decks-list";
 
 export default function IndexScreen() {
 	return (
 		<SafeAreaView style={styles.container}>
-			<AccordianList />
 			<DecksList />
 			<FloatingMenu />
 		</SafeAreaView>
